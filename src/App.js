5fae3842eb977130f33169071be00b892576c4bf
Gianluca <brivad@gmail.com>
2018-06-29 12:07:46 +0200

    add css and img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
+import logo from './logo.svg'
+import './index.css'
+
 export default class App extends Component {
   render() {
-    return <div>React App</div>
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <img src={logo} />
+      </div>
+    )
   }
 }
