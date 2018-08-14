7fe3a8e9f324efefa96de1657b0deb8f3a03feef
Gianluca <brivad@gmail.com>
2018-08-14 15:01:01 +0200

    add a component and update packages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './index.scss'
 
+import SuperList from './SuperList'
+
+const items = [
+  { id: 1, name: 'mario' },
+  { id: 2, name: 'ciccio' },
+  { id: 3, name: 'eva' }
+]
+
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React App</h1>
         <img src={logo} />
+        <SuperList items={items} />
       </div>
     )
   }
---
+++ src/SuperList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class SuperList extends Component {
+  state = { items: this.props.items }
+
+  updateItem = item => {
+    this.setState(state => ({
+      items: state.items.map(
+        _item =>
+          _item.id === item.id
+            ? { ...item, name: item.name.toUpperCase() }
+            : item
+      )
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Super List</h2>
+
+        {this.state.items.map(item => (
+          <div key={item.id} onClick={() => this.updateItem(item)}>
+            {item.id} - {item.name}
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
